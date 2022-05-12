252544dca7a75c78b066d311829a6494689e4b55
Valentyn Tishcheko <valentyntishchenko@gmail.com>
2022-05-12 21:52:53 +0300

    Fixed fotns

--- src/app/init-form.js
+++ src/app/init-form.js
@@ -1,3 +1,13 @@
+const resetFeedBackFrom = () => {
+  document.getElementById('customer-number').value = '';
+  document.getElementById('customer-email').value = '';
+};
+
+const sendFeedback = () => {
+  const customerNumber = document.getElementById('customer-number').value;
+  const customerEmail = document.getElementById('customer-email').value;
+  
+};
 export const initForm = () => {
-  console.log('initForm');
-}
+  document.getElementById('send-feedback-button').addEventListener("click", sendFeedback);
+};
