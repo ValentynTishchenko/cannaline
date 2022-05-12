af7b88b17635751e6ccce1fee64e84199dece5c4
Valentyn Tishcheko <valentyntishchenko@gmail.com>
2022-05-13 00:01:30 +0300

    staarted working on form

--- src/app/init-form.js
+++ src/app/init-form.js
@@ -1,12 +1,50 @@
+import axios from 'axios';
+import validator from 'validator';
+
 const resetFeedBackFrom = () => {
   document.getElementById('customer-number').value = '';
   document.getElementById('customer-email').value = '';
+  document.getElementById('customer-comment').value = '';
 };
 
-const sendFeedback = () => {
-  const customerNumber = document.getElementById('customer-number').value;
-  const customerEmail = document.getElementById('customer-email').value;
-  
+const URL = 'https://cannaline-email-sender.herokuapp.com/api/send-email';
+
+const sendFeedback = async (e) => {
+  e.preventDefault();
+
+  const number = document.getElementById('customer-number').value;
+  const email = document.getElementById('customer-email').value;
+  const comment = document.getElementById('customer-comment').value;
+
+  let isValid = true;
+
+  if (!validator.isEmail(email)) {
+    console.log('isEmail');
+    isValid = false;
+  }
+
+  if (!validator.isMobilePhone(number, 'uk-UA')) {
+    console.log('isMobilePhone');
+    isValid = false;
+  }
+
+  if (validator.isEmpty(comment)) {
+    console.log('isEmpty');
+    isValid = false;
+  }
+
+  if (!isValid) {
+    return;
+  }
+
+  try {
+    console.log(await axios.post(URL, {number, email, comment}));
+  } catch (e) {
+    console.log('error: ', e);
+    return;
+  }
+
+  console.log('sent');
 };
 export const initForm = () => {
   document.getElementById('send-feedback-button').addEventListener("click", sendFeedback);
